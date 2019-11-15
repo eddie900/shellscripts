ba4f229da7ce4bebbb5ad6a062f33be7f11f9d64
eddie <eddiecher@gmail.com>
2019-11-15 13:07:23 -0800

    Added RAID to diskconfig.sh

--- diskconfig.sh
+++ diskconfig.sh
@@ -12,5 +12,7 @@ fdisk -l | head -17 >>$FLUFFY_BUNNY.report
 
 echo -e "\n\n Mounted Filesystems: \n\n" >>$FLUFFY_BUNNY.report
 df -hT | grep -v tmp >>$FLUFFY_BUNNY.report
+echo -e "\n\n RAID Configuration: \n\n" >>$FILENAME.report
+mdadm --detail /dev/md0 >>$FILENAME.report
 
 
